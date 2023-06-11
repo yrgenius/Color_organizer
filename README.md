88df25d6269b64734a98f791d5021ec02f44caed
yrgenius <yrgenius80@gmail.com>
2023-06-11 23:27:22 +0900

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# Color organizer
-
-<!-- [Deploy (ссылка кликабельна)](https://yrgenius.github.io/Color_organizer/src/) -->
-
-![](./screen.png)
--- src/components/Star.jsx
+++
@@ -1,9 +0,0 @@
-import '../styles/star.css';
-
-const Star = ({ selected = 0, onClick = f => f }) => {
-    return (
-        <div className={(selected) ? 'star selected' : 'star'} onClick={onClick} ></div >
-    );
-}
-
-export { Star };
